4d40128416e4659c6b943160516c0c626e1ddca9
Đorđije Kundačina <qndaa98@gmail.com>
2025-09-29 15:45:16 +0200

    V1

---
+++ src/main.rs
@@ -0,0 +1,56 @@
+use std::error::Error;
+use std::{env, fs, process};
+use minigrep::{search, search_case_insensitive};
+
+struct Config {
+    query: String,
+    file_path: String,
+    ignore_case: bool
+}
+
+impl Config {
+    fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("Not enough arguments!");
+        }
+
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+        let ignore_case = env::var("IGNORE_CASE").is_ok();
+
+        Ok(Config { query, file_path, ignore_case })
+    }
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
+
+    println!("Searching for {0}", config.query);
+    println!("In file {0}", config.file_path);
+
+    if let Err(e) = run(config) {
+        eprintln!("Application error: {e}");
+        process::exit(1);
+    }
+}
+
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let content = fs::read_to_string(config.file_path)?;
+
+    let results = if config.ignore_case {
+        search_case_insensitive(&config.query, &content)
+    } else {
+        search(&config.query, &content)
+    };
+
+    for line in results {
+        println!("{line}");
+    }
+
+    Ok(())
+}
